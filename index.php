9e7220260a82f4f10878235881487d4800628574
Scapulus <simon_galle@gmx.de>
2023-11-15 16:36:31 +0100

    update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <meta charset="utf-8">
-        <title>Login</title>
+        <meta content="width=device-width, initial-scale=1.0">
+        <title>ToDo-List - Login</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://kit.fontawesome.com/ffb3484693.js" crossorigin="anonymous"></script>
     </head>
     <body>
     <?php
@@ -13,11 +15,39 @@
 
         }
     ?>
-    <h1>Anmelden</h1>
-    <form action="index.php" method="post">
-        <input type="text" name="email" placeholder="E-Mail" required><br>
-        <input type="password" name="password" placeholder="Passwort" required><br>
-        <button type="submit" name="submit">Einloggen</button>
-    </form>
+    <header>
+        <h1 class="logo">
+            <i class="fa-regular fa-square-check"></i>
+            ToDo-List
+        </h1>
+        <nav class="navigation">
+            <a href="#">Home</a>
+            <a href="#">Registrieren</a>
+        </nav>
+    </header>
+    <div class="container">
+        <div class="center-box">
+            <h2>Login</h2>
+            <form action="index.php" method="post">
+                <div class="input-group">
+                    <div class="input-field">
+                        <i class="fa-regular fa-envelope"></i>
+                        <input type="text" name="email" placeholder="E-Mail" required><br>
+                    </div>
+                    <!--div class="input-field">
+                        <i class="fa-regular fa-user"></i>
+                        <input type="text" name="username" placeholder="Benutzername" required><br>
+                    </div-->
+                    <div class="input-field">
+                        <i class="fa-solid fa-lock"></i>
+                        <input type="password" name="password" placeholder="Passwort" required><br>
+                    </div>
+                </div>
+                <div class="button-field">
+                    <button type="submit" name="submit">Anmelden</button>
+                </div>
+            </form>
+        </div>
+    </div>
     </body>
 </html>
